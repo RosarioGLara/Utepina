0e42da9723644334441cb2a5e7cbf4f754d16ff2
RosarioGLara <rglara2@miners.utep.edu>
2025-03-08 22:43:55 -0700

    Create PalindromeSolution.java
    isPalindrome solution exercise in java Leetcode.

---
+++ PalindromeSolution.java
@@ -0,0 +1,19 @@
+class PalindromeSolution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0){
+            return true;
+        }
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        int start = 0;
+        int end = s.length()-1;
+        while(start <= end){
+            if(s.charAt(start) != s.charAt(end)){
+                return false;
+            }
+            start ++;
+            end --;
+        }
+        return true;
+    }
+}
